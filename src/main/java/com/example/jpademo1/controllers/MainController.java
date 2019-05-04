55ac7bab95d15622fe1142f85cb17ebd6314985f
Michal-Clever <inz.michal.milczarek@gmail.com>
2019-05-04 20:10:16 +0200

    Post

--- src/main/java/com/example/jpademo1/controllers/MainController.java
+++ src/main/java/com/example/jpademo1/controllers/MainController.java
@@ -4,7 +4,10 @@ import com.example.jpademo1.models.UserEntity;
 import com.example.jpademo1.models.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Optional;
@@ -34,10 +37,40 @@ public class MainController {
     @ResponseBody
     public String index() {
         // jedego użytkownika po ID
-         Optional<UserEntity> downData = userRepository.findById(10);
-     //   wywala błąd przy braku id
-         return downData.orElseThrow(IllegalStateException::new).toString();
+        Optional<UserEntity> downData = userRepository.findById(10);
+        //   wywala błąd przy braku id
+        return downData.orElseThrow(IllegalStateException::new).toString();
+    }
 
+
+    @GetMapping("/register")
+    public String register() {
+        return "register";
+    }
+
+    @PostMapping("/register")
+    //  @ResponseBody
+    public String register(@RequestParam("login") String login,
+                           @RequestParam("password") String password,
+                           @RequestParam("passwordReapet") String passwordReapet,
+                           @RequestParam("email") String email,
+                           Model model) {
+        if (!password.equals(passwordReapet)) {
+            //  return "Hasła nie są takie same";
+            model.addAttribute("info", "Hasła się nie zgadzają!");
+            return "register";
+        }
+
+        UserEntity userEntity = new UserEntity();
+        userEntity.setPassword(password);
+        userEntity.setEmail(email);
+        userEntity.setUsername(login);
+
+        userRepository.save(userEntity);
+
+        //return "Konto utworzone";
+        model.addAttribute("info", "Zarejestrowano :D");
+        return "register";
 
     }
 
